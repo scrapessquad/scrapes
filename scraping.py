a9d925c3266fe15db7118a2d3bf7d9e8beed1315
Bradley Fargo <blewfargs@gmail.com>
2019-02-26 15:59:43 -0500

    initializing new file dope

---
+++ scraping.py
@@ -0,0 +1,19 @@
+'''
+Project Starter Code
+
+scraping.py
+'''
+
+
+'''
+- tell python to visit a website ex:
+https://huntnewsnu.com/57940/campus/ken-henderson-named-first-chancellor-of-nu/
+- tell python to get a certain thing from that website (go to class storycontent for huntnews stuff)
+- prepocess that stuff (get rid of <p> tags)
+- we want to end up with a list of words (so like spaces removed
+['example', 'text', 'fun'] each word is individual string
+- get rid of fluff words (using NLTK)
+- put into wordcloud  -- piotr knows how
+
+
+'''
