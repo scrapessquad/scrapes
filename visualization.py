59939353765cd2b82f8b52ddd083fc68351a38a7
Bradley Fargo <blewfargs@gmail.com>
2019-03-05 20:44:02 -0500

    create skeleton of file

---
+++ visualization.py
@@ -0,0 +1,19 @@
+'''
+Project Starter Code
+visualization.py
+'''
+
+'''
+file will be all about how to analyze the data, present graphs
+or wordclouds using matlib or turtle or whatever
+'''
+
+def lorem(ipsum)
+  '''
+  Params: 
+  Return: 
+  Describe: 
+  '''
+  print('lorem ipsum')
+ 
+lorem('ispum')
